32dc3136599778cc4357a70c35a33d5ecc8fd1d1
阿偉特 <aa4192696@gmail.com>
2023-11-21 16:00:56 +0800

    update

--- src/main/java/com/example/login/LoginServlet.java
+++ src/main/java/com/example/login/LoginServlet.java
@@ -12,6 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet{
 
 	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		super.doPost(req, resp);
